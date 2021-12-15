0e13a1465dd07afdd7e26f5f11e383c1892a53e7
NarguSabz <78127649+NarguSabz@users.noreply.github.com>
2021-12-14 23:57:03 -0500

    Update MainWindow.xaml.cs

--- PomodoroTimer/MainWindow.xaml.cs
+++ PomodoroTimer/MainWindow.xaml.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+*
+* Travail pratique #3
+* Sabbagh Ziarani, Narges
+* Date: 14 decembre 2021
+* Description: pomodoro.
+*
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -21,20 +29,33 @@ namespace PomodoroTimer
 {
     public partial class MainWindow : Window
     {
-
-        public static RoutedCommand cmdDemarrerPomodoro = new RoutedCommand();
-        public static RoutedCommand cmdInterromprePomodoro = new RoutedCommand();
+        //Objet statique utilise pour gerer la commande demarrer qui permet de demarrer un pomodoro
+        public static RoutedCommand CmdDemarrerPomodoro = new RoutedCommand();
+        //Objet statique utilise pour gerer la commande interrompre
+        public static RoutedCommand CmdInterromprePomodoro = new RoutedCommand();
+        //Objet statique utilise pour gerer la commande Ajouter une tache
+        public static RoutedCommand CmdAjouterTache = new RoutedCommand();
+        //Objet statique utilise pour gerer la commande ajouter un pomodoro
+        public static RoutedCommand CmdAjouterNombrePomodoro = new RoutedCommand();
+        //Objet statique utilise pour gerer la commande diminuer un pomodoro
+        public static RoutedCommand CmdDiminuerNombrePomodoro = new RoutedCommand();
+        //Objet statique utilise pour gerer la commande supprimmer une tache
+        public static RoutedCommand CmdSupprimerTache = new RoutedCommand();
 
         private const int NOMBRE_SECONDES = 60;
         private BackgroundWorker _tempsPomodoro;
+        private ViewModel viewModel;
+
 
         public MainWindow()
         {
+            viewModel = new ViewModel();
             InitializeComponent();
+            DataContext = viewModel;
         }
 
         // Début du code pour la minuterie, donné aux étudiants
-		// Il devra être adapté aux spécifications du travail
+        // Il devra être adapté aux spécifications du travail
         private void DemarrerPomodoro()
         {
             if (_tempsPomodoro != null && _tempsPomodoro.IsBusy)
@@ -62,7 +83,7 @@ namespace PomodoroTimer
 
             BackgroundWorker worker = sender as BackgroundWorker;
 
-            while (secondes > 0 && ! worker.CancellationPending)
+            while (secondes > 0 && !worker.CancellationPending)
             {
                 Thread.Sleep(1000);
                 secondes--;
@@ -84,7 +105,16 @@ namespace PomodoroTimer
 
         private void TerminerPomodoro(object sender, RunWorkerCompletedEventArgs e)
         {
-            MessageBox.Show("Pomodoro terminé");
+            //si l utilisateur repond oui a cette question on ajoute le nombre de pomodoro completes de la tache
+            if (MessageBox.Show("Est-ce que le pomodoro a été complétée?", "Temps expiré", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                viewModel.MettreAJourPomodoro();
+            }
+            //si l utilisateur repond oui a cette question on dit que la tache a ete complete, en faisant appel a la methode MettreAJOurTAche de la classe viewModel
+            if (MessageBox.Show("Est-ce que la tâche a été complétée?", "Temps expiré", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                viewModel.MettreAJourTache();
+            }
         }
 
         private void InterromprePomodoro()
@@ -96,29 +126,84 @@ namespace PomodoroTimer
                 _tempsPomodoro.CancelAsync();
             }
             AfficherTemps(0);
-            MessageBox.Show("Pomodoro interrompu");
-        }
+            //si l utilisateur repond oui a cette question on dit que la tache a ete complete, en faisant appel a la methode MettreAJOurTAche de la classe viewModel
+            if (MessageBox.Show("Est-ce que la tâche a été complétée?", "Temps expiré", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                viewModel.MettreAJourTache();
+            }
 
+        }
+        //canExecute de la commande demarrer s active si la tache n est pas encore completee
         private void CommandedDemarrerPomodoro_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-            e.CanExecute = true;
+            e.CanExecute = StatutTache != null && StatutTache.Text != "Complétée" && StatutTache.Text != "";
         }
-
+        //execute de la commande demarrer
         private void CommandedDemarrerPomodoro_Executed(object sender, ExecutedRoutedEventArgs e)
         {
+            //fait appel a la methode DemarrerPomodoros de l objet viewModel
+            viewModel.DemarrerPomodoros();
             DemarrerPomodoro();
         }
-
+        //canexecute de la commande interrompre
+        //canExecute est mis a vrai tant qu il y a la minutrie
         private void CommandeInterromprePomodoro_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
             e.CanExecute = _tempsPomodoro != null && _tempsPomodoro.IsBusy;
         }
 
+        //execute de la commande interrompre
         private void CommandeInterromprePomodoro_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             InterromprePomodoro();
         }
         // Fin du code relié à la minuterie 
+
+        //can execute de la commande ajouterpomodoro
+        private void CmdAjouterNombrePomodoro_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = NombrePomodoros != null;
+        }
+        //execute de la commande ajouter pomodoro, tant que champ NombrePomodoros existe celui ci peut etre execute
+        private void CmdAjouterNombrePomodoro_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            NombrePomodoros.Text = "" + (int.Parse(NombrePomodoros.Text) + 1);
+        }
+        //can execute de la commande diminuerpomodoro
+
+        private void CmdDiminuerNombrePomodoro_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = NombrePomodoros != null && int.Parse(NombrePomodoros.Text) != 0;
+        }
+        //execute de la commande diminuerpomodoro, tant que le nombre de pomodoros est tout autre que 0
+
+        private void CmdDiminuerNombrePomodoro_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            NombrePomodoros.Text = "" + (int.Parse(NombrePomodoros.Text) - 1);
+        }
+        //can execute de la commande ajouter tache
+        private void CmdAjouterTache_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = NombrePomodoros != null && DescriptionTache != null && int.Parse(NombrePomodoros.Text) != 0 && DescriptionTache.Text != "";
+        }
+        //execute de la commande tache, tant que il y a une description et le nombre de pomodoros prevue est superieur a 0 elle peut etre executee
+        private void CmdAjouterTache_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            viewModel.AjouterTache(DescriptionTache.Text, NombrePomodoros.Text);
+        }
+        //can execute de la commande supprimer tache
+        private void CmdSupprimerTache_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+
+            e.CanExecute = ListboxTaches != null && ListboxTaches.SelectedIndex >= 0;
+
+        }
+        //execute de la commande supprimer, tant que la tache selectionne est valide on peut le supprimer
+        private void CmdSupprimerTache_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            //fait appel au methode supprimertache de  l objet viewModel
+            viewModel.SupprimerTache();
+        }
 
     }
 }
